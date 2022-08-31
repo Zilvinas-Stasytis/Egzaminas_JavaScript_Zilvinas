7a368bb3e7c5d89b8d1142a4aa99a388a43c288b
Zilvinas <zalciukas@gmail.com>
2022-08-31 22:34:11 +0300

    padaryta

--- JS Tasks/Task 9/script.js
+++ JS Tasks/Task 9/script.js
@@ -10,24 +10,17 @@ Metodas:
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
 class Movie {
-  constructor(title, director, budget, cost) {
+  constructor(title, director, budget) {
     (this.title = title),
       (this.director = director),
-      (this.budget = budget),
-      (this.cost = cost);
-    function wasExpensive(budget) {
-      if (budget > 100000000) {
-        cost = true;
-      } else {
-        cost = false;
-      }
-      return cost;
+      (this.budget = budget)    
     }
+    wasExpensive() {
+      return this.budget > 100000000? true:false;
   }
 }
 
 const movie1 = new Movie("Bladerunner", "Ridley Scott", 25000);
-movie.wasExpensive(25000);
-const movie2 = new Movie("Birds", "Alfred Hitchcock", 255000);
-
-console.log(movie2.cost);
+console.log(movie1.wasExpensive());
+const movie2 = new Movie("Birds", "Alfred Hitchcock", 255000000);
+console.log(movie2.wasExpensive());
