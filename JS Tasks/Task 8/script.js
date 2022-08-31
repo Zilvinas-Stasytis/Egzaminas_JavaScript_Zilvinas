2e90a73eaed14eced7d5a6f30959fc0db2a2c29a
Zilvinas <zalciukas@gmail.com>
2022-08-31 22:15:29 +0300

    Dar daugiau

--- JS Tasks/Task 8/script.js
+++ JS Tasks/Task 8/script.js
@@ -5,3 +5,29 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+class Calculator {
+  constructor(a, b) {
+    this.a = a;
+    this.b = b;
+  }
+  sum() {
+    return this.a + this.b;
+  }
+  subtraction() {
+    return this.a - this.b;
+  } 
+  multiplication() {
+    return this.a * this.b;
+  } 
+  division() {
+    return this.a / this.b;
+  }  
+}
+const suma = new Calculator(5, 6);
+console.log(suma.sum());
+const skirtumas = new Calculator(5, 6);
+console.log(suma.subtraction());
+const sandauga = new Calculator(5, 6);
+console.log(suma.multiplication());
+const dalmuo = new Calculator(5, 6);
+console.log(suma.division().toFixed(4));
