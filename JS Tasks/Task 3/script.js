8742ae6caba9158626d369cc07c87d46fc6c4a1d
Zilvinas <zalciukas@gmail.com>
2022-08-31 21:19:57 +0300

    Tolimesni veiksmai

--- JS Tasks/Task 3/script.js
+++ JS Tasks/Task 3/script.js
@@ -11,25 +11,22 @@ Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
-document.getElementById('btn').addEventListener('click', obtenirUtilisateur);
+const ENDPOINT = "https://api.github.com/users";
+document.getElementById("btn").addEventListener("click", obtenirUtilisateur);
 function obtenirUtilisateur(event) {
-    event.preventDefault();
-    fetch(ENDPOINT)
-        .then((response) => response.json())
-        .then((data) => lirePersonne(data));
-
-
+  event.preventDefault();
+  fetch(ENDPOINT)
+    .then((response) => response.json())
+    .then((data) => lirePersonne(data));
 }
-function lirePersonne(personne) {
-    let root = document.getElementById('output');
-    for (let i = 0; i < personne.length; i++) {
-        console.log(personne);
-        let githubUser = document.createElement('div');
-       Element.className = 'githubUsers';
-        root.appendChild(githubUser);
-        
-        document.getElementsByClassName("githubUsers").innerText = personne[i].login;
-        // document.getElementById("output").innerText = person[0].avatar_url;
-    }
+function lirePersonne(personnes) {
+  let root = document.getElementById("output");
+  root.textContent = "";
+  console.log(personnes);
+  for (let i = 0; i < personnes.length; i++) {
+    let githubUser = document.createElement("p");
+    githubUser.className = "githubUsers";
+    root.appendChild(githubUser);
+    githubUser.innerText = personnes[i].login +"       "+ personnes[i].avatar_url;
+  }
 }
