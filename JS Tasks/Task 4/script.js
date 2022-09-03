12e892947eb3461cbabe3355b464618ab48c5466
Zilvinas <zalciukas@gmail.com>
2022-09-03 10:55:57 +0300

    Finalinis variantas tikrai

--- JS Tasks/Task 4/script.js
+++ JS Tasks/Task 4/script.js
@@ -19,29 +19,29 @@ fetch(ENDPOINT)
 function lireVoitures(voitures) {
   let root = document.getElementById("output");
   for (let i = 0; i < voitures.length; i++) {
-    // for (j = 0; j < voitures[i].models.length; j++) {
-      const card = document.createElement("div");
-      const divImage = document.createElement("div");
-      const cardImg = document.createElement("img");
-      const divPara = document.createElement("div");
-      const paragraph1 = document.createElement("p");
-      const paragraph2 = document.createElement("p");
-      const modelList = document.createElement("select");
-    //   const modelName = voitures[i].models[1];
-      card.append(divImage, divPara);
-      divImage.append(cardImg);
-      divPara.append(paragraph1, paragraph2, modelList);
-      root.append(card);
+    const card = document.createElement("div");
+    const divImage = document.createElement("div");
+    const cardImg = document.createElement("img");
+    const divPara = document.createElement("div");
+    const paragraph1 = document.createElement("p");
+    const paragraph2 = document.createElement("p");
+    const modelList = document.createElement("select");
+     for (j = 0; j < voitures[i].models.length; j++) {
+    const modelName = document.createElement("option"); 
+    modelName.textContent=voitures[i].models[j];
+    modelList.appendChild(modelName);
+      }
+    card.append(divImage, divPara);
+    divImage.append(cardImg);
+    divPara.append(paragraph1, paragraph2, modelList);
+    root.append(card);
 
-      card.className = "card";
-      divImage.className = "img";
-      divPara.className = "card-footer";
-      paragraph1.className = "voiture";
-      modelList.className = "models";
-      paragraph1.textContent = voitures[i].brand;
-      paragraph2.innerText = "Models of this brand";
-    //   modelList.options.value = modelName[j];
-    
-    // }
-  }
+    card.className = "card";
+    divImage.className = "img";
+    divPara.className = "card-footer";
+    paragraph1.className = "voiture";
+    modelList.className = "models";
+    paragraph1.textContent = voitures[i].brand;
+    paragraph2.innerText = "Models of this brand";
+      }
 }
